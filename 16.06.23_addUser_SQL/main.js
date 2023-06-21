fd82e9045de4a4339084ac35ded770b57e0ee924
nodie908 <nikitios641@gmail.com>
2023-06-21 21:56:41 +0600

    Revert "16.06.23_addUser_SQL"
    This reverts commit cd01d985683f051a27d4abeb574bf6b2e98768be.

--- 16.06.23_addUser_SQL/main.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "./bin/app.js";
-import fs from 'fs';
-import ini from 'ini';
-
-const cfg_file = fs.readFileSync("./config.ini", "utf-8");
-const config = ini.parse(cfg_file);
-
-const app = new Application(config);
-app.start();
